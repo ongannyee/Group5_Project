806a66a79c9846fb07d461403e8476c570438376
Mavu02 <160746246+Mavu02@users.noreply.github.com>
2025-06-03 00:43:48 +0800

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 1f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("What was that?!");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
+    }
+}
