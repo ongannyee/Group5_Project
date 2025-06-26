a21431d7b054b131ccf78e1c30934584a4dfbcf5
ongannyee <ongannyee@gmail.com>
2025-06-26 21:37:26 +0800

    minor

--- Assets/Scripts/UI/StateChange.cs
+++ Assets/Scripts/UI/StateChange.cs
@@ -1,4 +1,4 @@
-/* using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -33,4 +33,3 @@ public class StateChange : MonoBehaviour
         }
     }
 }
- */
